b3e41784b0c13c41a9e4b7f6ef382856e24e6522
hy2021-IT <hkx5049@psu.edu>
2022-10-06 16:35:38 -0400

    last
    pre

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-library(shiny)
+
 library(fields)
 library(tidyverse)
 library(shiny)
@@ -11,7 +11,7 @@ library(dplyr)
 library(tidyselect)
 
 
-data(FruitFlies2)
+
 data(iris) # ciation for iris
 #load file
 Kiterfunction <- readRDS("Kiterfunction.rds")
@@ -26,7 +26,7 @@ ui <- list(
     skin = "red",
     ### Create the app header ----
     dashboardHeader(
-      title = "k_means Clustering", # You may use a shortened form of the title here
+      title = "k-means Clustering", # You may use a shortened form of the title here
       titleWidth = 250,
       tags$li(class = "dropdown", actionLink("info", icon("info"))),
       tags$li(
@@ -46,8 +46,9 @@ ui <- list(
       sidebarMenu(
         id = "pages",
         menuItem("Overview", tabName = "overview", icon = icon("dashboard")),
+        menuItem("Prerequisites", tabName = "Prerequisites", icon = icon("book")),
         menuItem("Examples", tabName = "example", icon = icon("book")),
-        menuItem("Explore", tabName = "explore", icon = icon("wpexplorer")),
+        #menuItem("Explore", tabName = "explore", icon = icon("wpexplorer")),
         menuItem("References", tabName = "references", icon = icon("leanpub"))
       ),
       tags$div(
@@ -62,11 +63,11 @@ ui <- list(
         tabItem(
           tabName = "overview",
           withMathJax(),
-          h1("Clustering App"), # This should be the full name.
+          h1(em("k"),"-means Clustering"), # This should be the full name.
           h2("M I S S I O N"),
-          p("Play now or load your data in the form below. Click the plot multiple times to identify new points and then click the 'Ready!' button to
-      visualize your score. Try to minimize the score with one, two and three points.Try to do it with more than three points. What is happening when increasing
-      the number of points?"),
+          p("Procced to the example page by clicking the Go Button below. If you have little
+            no background with k-means_clustering, proceed to the Prerequisites Page for
+            more relevent information."),
           div(
             style = "text-align: center;",
             bsButton(
@@ -81,8 +82,7 @@ ui <- list(
           br(),
           h2("Acknowledgements"),
           p(
-            "The orginial version of the app was developed and coded by Jacopo
-            Di Iorio. This version of the app was modified and coded by Hongyi Xia",
+            "The orginial version of the app was developed and coded by Hongyi Xia",
             br(),
             "I would like to acknowledge and appreciate Professor Hatfield, 
             Professor Pearl, and Professor Jacopo for their guidance and 
@@ -97,15 +97,88 @@ ui <- list(
             div(class = "updated", "Last Update: 6/13/2022 by Hongyi Xia.")
           )
         ),
+        
+        tabItem(
+          tabName = "Prerequisites",
+          withMathJax(),
+          h2("Prerequisites"), 
+          p("Review information about the k_means clustering."), 
+          br(), 
+          box(
+            width = 12, 
+            collapsible = TRUE,
+            collapsed = FALSE,
+            title = h3("Clustering"), 
+            p("Definition: Clustering is the general unsupervised task of grouping 
+              a set of objects in such a way that objects in the same group (called a cluster) 
+              are more similar to each other than to those in other groups (clusters)."), 
+            p("It can be achieved by various algorithms. Broadly speaking, according 
+              to the group partition obtained we can divide clustering methods in two groups:"), 
+            p("Hard Clustering: each data point belongs to only one cluster."),
+            p("Soft Clustering: for each point we have a probability or likelihood 
+              to be assigned in a cluster. In some algorithm it is also possible to 
+              identify overlapping clusters"),
+            p("We are going to focus mainly on exhaustive (every data point is assigned) 
+              hard clustering algorithms, k-means")
+          ), 
+          box(
+            width = 12, 
+            collapsible = TRUE,
+            collapsed = FALSE,
+            title = h3("K-means Algorithm"), 
+            p("k -means Clustering is a partitioning algorithm that splits the data in 
+              k clusters by iteratively computing centroids and moving data points to 
+              the closest centroids until convergence."), 
+            p("For determining the distance between data points and centrods in this k_means app,
+              Euclidean distance is applied by default, defined 
+              by"),
+            withMathJax(helpText('$$ \\sum_{i=1}^n \\sqrt{(x_i - y_i)^2} $$'))
+            
+            
+          ), 
+          box(
+            width = 12, 
+            collapsible = TRUE,
+            collapsed = FALSE,
+            title = h3("Total within cluster sum of squares"), 
+            p("The within-cluster sum of squares is a measure of the variability 
+              of the observations within each cluster. In general, a cluster that 
+              has a small sum of squares is more compact than a cluster that has 
+              a _large sum of squares. As the number of observations increases, 
+              the sum of squares becomes larger, so it depends on the number of elements."), 
+            p("In general we can sum the within-cluster sum of squares to get the 
+              total within cluster sum of square that cna be used to assess the 
+              goodness of our clustering."),
+            p("We also canidentify the optimal number of clusters with the Total within cluster 
+              sum of squares by looking for an elbow in the Total within cluster 
+              sum of squares versus number of clusters plot (provided to you in
+              the example page")
+            
+          ),
+          
+          br(), 
+          ##### Go Button
+          div(
+            style = "text-align: center",
+            bsButton(
+              inputId = "goExplore",
+              label = "GO!",
+              size = "large",
+              icon = icon("bolt"),
+              style = "default"
+            )
+          )
+        ), 
       
       ### set up example page
       tabItem(
         tabName = "example",
         withMathJax(),
         h2("Illustrative example of K-means with 3 centroids"),
-        p("Check out the siders with a play button to see iterative changes of cluster run by K-mean
-          Algorithm. An stacked barplot is accompanied to demonstrate the changes of total withiness 
-          as iterations progress."),
+        p("Check out the slider with a play button to see changes in the cluster 
+          centroids with iterations of the K-means algorithm. The stacked barplot 
+          that accompanies the centroids plot show changes in the within-group 
+          distances as iterations progress."),
         fluidRow(
           column(
             width = 4,
@@ -122,16 +195,8 @@ ui <- list(
                 animate = animationOptions(interval = 1500)
               )
             ),
-            p("Notice: The color of the data points change at the background based on the
-              distance to the centroid they are cloest to. For example, the green data points
-              have the closest distance to the green centroid, and that is why they have colored
-              in green. After the data points changes color, the position of centroids change
-              as well. The new centroid position will take on the mean x-coordinates and mean
-              y-coordinates of data points that the centroid has the same color with. For example, 
-              the new position of green centroid will move to the center of cluster of data points
-              colored in green. Then, a new round of coloring of data points start over again. These
-              iterative step carry on until there is no more change in the coloring of data points and 
-              position of the centroids")
+            uiOutput(outputId = "dataDescription"),
+            p(".")
           ),
           column(
             width = 8,
@@ -176,7 +241,6 @@ ui <- list(
           column(
             width = 8,
             offset = 0,
-            uiOutput(outputId = "dataDescription"),
             br(),
             plotOutput(outputId = "plot2",click = "plot_click"),
             plotOutput(outputId = "plotscatter",click = "plot_click")
@@ -224,9 +288,9 @@ ui <- list(
           ),
           p(
             class = "hangingindent",
-            "Perrier, V., Meyer, F., and Granjon, D. (2022). shinyWidgets: Custom
-            inputs widgets for shiny. (v 0.7.0). [R package]. Available from
-            https://CRAN.R-project.org/package=shinyWidgets"
+            "Henry, L., Wickham, H., RStudio (2022). tidyselect: Select from a 
+            Set of Strings. (v 1.1.2) [R package]. Available from
+             https://cran.r-project.org/web/packages/tidyselect/index.html"
           ),
           p(
             class = "hangingindent",
@@ -235,6 +299,14 @@ ui <- list(
             fields: Tools for Spatial Data. (v 13.0.0). [R package]. Available from
             https://CRAN.R-project.org/package=fields"
           ),
+          p(
+            class = "hangingindent",
+           "Perrier, V., Meyer, F., and Granjon, D. (2022). shinyWidgets: Custom
+            inputs widgets for shiny. (v 0.7.0). [R package]. Available from
+            https://CRAN.R-project.org/package=shinyWidgets"
+          ),
+          
+  
           br(),
           br(),
           br(),
@@ -249,6 +321,28 @@ ui <- list(
 
 server <- function(input, output, session) {
   
+  ## data description
+  observeEvent(
+    eventExpr = input$Exiter,
+    handlerExpr = {
+      description <- switch(
+        EXPR = ((input$Exiter)%%1)*2+1,
+        "After the data points changes color, the position of centroids change
+         as well. The new centroid position will take on the mean x-coordinates and mean
+         y-coordinates of data points that the centroid has the same color with. For example, 
+         the new position of green centroid will move to the center of cluster of data points
+         colored in green. Then, a new round of coloring of data points start over again. These
+         iterative step carry on until there is no more change in the coloring of data points and 
+         position of the centroids",
+        "Notice: The color of the data points change at the background based on the
+         distance to the centroid they are cloest to. For example, the green data points
+         have the closest distance to the green centroid."
+      )
+      
+      output$dataDescription <- renderUI(description)
+    }
+  )
+  
  
   
   ## Set Buttons
@@ -258,7 +352,7 @@ server <- function(input, output, session) {
       updateTabItems(
         session = session,
         inputId = "pages",
-        selected = "explore"
+        selected = "example"
       )
     }
   )
@@ -441,7 +535,11 @@ server <- function(input, output, session) {
                aes(x=Sepal.Length,y=Sepal.Width), color="black") +
         geom_point(alpha=1-scale) +
         theme_bw() +
-        scale_color_manual(values = boastUtils::psuPalette)
+        scale_color_manual(values = boastUtils::psuPalette) +
+        guides(color =  guide_legend(title = "Cluster Grouping")) +
+        theme(legend.title = element_text(size = 20)) +
+        theme(legend.text = element_text(size = 20)) +
+        theme(axis.title = element_text(size = 20)) 
       
       if (k == 0.5){
         outputPlot <- basePlot
@@ -482,7 +580,13 @@ server <- function(input, output, session) {
              geom_bar(position="stack", stat="identity") +
              geom_line(data=totData, aes(x=iteration, y=total), inherit.aes = FALSE) +
              scale_fill_manual(values = boastUtils::psuPalette) +
-             theme_bw()
+             theme_bw() +
+             ylab("Totla Within Sum of Squares") +
+             theme(legend.title = element_text(size = 20)) +
+             theme(legend.text = element_text(size = 20)) +
+             theme(axis.title = element_text(size = 20)) +
+             scale_x_continuous(breaks = seq(1,10,1))
+             
       
       output$Exbarplot <- renderPlot(expr = {barOutput})
       
